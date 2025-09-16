7d96864477cee596009f6421b8cdaf37d2abb1dc
Manuel Gruber <m.gruber1@htlkrems.at>
2025-09-16 09:14:46 +0200

    struktur

---
+++ LiftArc/LiftArc/Program.cs
@@ -0,0 +1,28 @@
+using LiftArc.Components;
+using LiftArc.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<WorkoutService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
